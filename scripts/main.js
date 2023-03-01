5ffb3851b5e54333e77fb545657ea4d6ab66495c
DEVArt1st <57113800+DEVArt1st@users.noreply.github.com>
2023-03-01 17:37:08 -0500

    Created Color Reference Table and Variant id Generator for PWC ONLY. The PWC that is currently only supported in this update is Sea-Doo

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,23 @@
+// SKU GENERATOR
 const skuNumberField = document.getElementById("skuNumberField");
 const updateSkuBtn = document.getElementById("skuNumberFieldSubmitBtn");
 const clearSkuInputFieldBtn = document.getElementById("skuNumberFieldClear");
 const skuValue = document.getElementsByClassName("sku-value");
 
-
+// TITLE GENERATOR
 const titleTextInputField = document.getElementById("titleTextField");
 const updateTitleBtn = document.getElementById("titleTextFieldSubmitBtn");
 const clearTitleFieldBtn = document.getElementById("titleTextFieldClear");
 const titleFill = document.getElementsByClassName("title-fill");
 
+
+//PRODUCT ID GENERATOR (SEA DOO)
+const seaDooTextInputField = document.getElementById("seaDooTextField");
+const updateSeaDooBtn = document.getElementById("seaDooFieldSubmitBtn");
+const clearSeaDooBtn = document.getElementById("seaDooFieldClear");
+const seaDooFill = document.getElementsByClassName("sea-doo-variant-id-fill");
+
+// TITLE GENERATOR
 function clearTitleFieldInput(){
     titleTextInputField.value = "";
 }
@@ -19,6 +28,7 @@ function updateTitle(){
     }
 }
 
+// SKU GENERATOR
 function clearSkuFieldInput(){
     skuNumberField.value = "";
 }
@@ -30,10 +40,26 @@ function updateSku(){
 }
 
 
-// TITLE FIELDS
+// SEA DOO PRODUCT ID GENERATOR
+function clearSeaDooFieldInput(){
+    seaDooTextInputField.value = "";
+}
+
+function updateSeaDoo(){
+    for(const seaDoo of seaDooFill){
+        seaDoo.innerHTML = seaDooTextInputField.value;
+    }
+}
+
+
+// TITLE FIELDS EVENTS
 updateTitleBtn.addEventListener("click", updateTitle);
 clearTitleFieldBtn.addEventListener("click", clearTitleFieldInput);
 
-// SKU FIELDS
+// SKU FIELDS EVENTS
 clearSkuInputFieldBtn.addEventListener("click", clearSkuFieldInput);
 updateSkuBtn.addEventListener("click", updateSku);
+
+// SEA DOO PRODUCT ID EVENTS
+clearSeaDooBtn.addEventListener("click", clearSeaDooFieldInput);
+updateSeaDooBtn.addEventListener("click", updateSeaDoo);
